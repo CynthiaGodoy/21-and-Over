8d6f7a207d2626ea07cd02c381b8009b03d7c605
Cynthia Godoy <cynthia@ck-interiors.com>
2022-11-22 11:40:04 -0700

    adjust background image

---
+++ js/age-gate.js
@@ -0,0 +1,11 @@
+var ageSection = document.querySelector(".agegate");
+
+    function closeAgegate() {
+        ageSection.style.display = "none";
+    }
+
+var failSection = document.querySelector(".fail");
+
+    function openFail() {
+        failSection.style.display = "block";
+    }
